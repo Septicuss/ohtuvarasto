e26d010e9bb44a41d3cf7021330037d28a228cfd
Septicuss <vbalaban55@gmail.com>
2025-11-01 12:27:10 +0200

    task: 8, unittest

--- src/tests/varasto_test.py
+++ src/tests/varasto_test.py
@@ -4,7 +4,15 @@ from varasto import Varasto
 
 class TestVarasto(unittest.TestCase):
     def setUp(self):
+        self.tilavuus = 10
+        self.alku_saldo_puolitaynna = 5
+        self.alku_saldo_ylitaynna = 20
+
         self.varasto = Varasto(10)
+        self.virheellinen_varasto = Varasto(-1, -1)
+        self.puolitaynna_varasto = Varasto(self.tilavuus, self.alku_saldo_puolitaynna)
+        self.taynna_varasto = Varasto(self.tilavuus, self.tilavuus)
+        self.ylitaynna_varasto = Varasto(self.tilavuus, self.alku_saldo_ylitaynna)
 
     def test_konstruktori_luo_tyhjan_varaston(self):
         # https://docs.python.org/3/library/unittest.html#unittest.TestCase.assertAlmostEqual
@@ -38,3 +46,37 @@ class TestVarasto(unittest.TestCase):
 
         # varastossa pitäisi olla tilaa 10 - 8 + 2 eli 4
         self.assertAlmostEqual(self.varasto.paljonko_mahtuu(), 4)
+
+    def test_virheellinen_tilavuus(self):
+        self.assertEqual(self.virheellinen_varasto.tilavuus, 0)
+
+    def test_virheellinen_alku_saldo(self):
+        self.assertEqual(self.virheellinen_varasto.saldo, 0)
+
+    def test_virheellinen_maara_ei_muuta_saldoa(self):
+        alku_maara = self.varasto.saldo
+
+        self.varasto.lisaa_varastoon(-1)
+
+        self.assertEqual(self.varasto.saldo, alku_maara)
+
+    def test_lisays_ei_ylita_tilavuutta(self):
+        self.varasto.lisaa_varastoon(self.alku_saldo_ylitaynna)
+
+        self.assertEqual(self.varasto.saldo, self.varasto.tilavuus)
+
+    def test_virheellinen_ottaminen(self):
+        saatu_maara = self.varasto.ota_varastosta(-1)
+
+        self.assertEqual(saatu_maara, 0.0)
+
+    def test_kaikki_mita_voidaan_ottaa(self):
+        alku_maara = self.varasto.saldo
+        saatu_maara = self.varasto.ota_varastosta(self.alku_saldo_ylitaynna)
+
+        self.assertEqual(saatu_maara, alku_maara)
+
+    def test_merkkijono(self):
+        odotettu = f"saldo = {self.alku_saldo_puolitaynna}, vielä tilaa {self.tilavuus - self.alku_saldo_puolitaynna}"
+
+        self.assertEqual(self.puolitaynna_varasto.__str__(), odotettu)
